2cc432776f8ad5c13eee595a640db4744c80d863
Atsushi Watanabe <atsushi.w@ieee.org>
2025-12-16 14:38:25 +0900

    Revert "Rename rosinstall file in test"
    This reverts commit 017adc203bfe209252fda3d2e00b5ea322dcaf71.

--- test/network_tests/network_test_utils.py
+++ test/network_tests/network_test_utils.py
@@ -18,7 +18,7 @@ class AbstractUnstableTest(AbstractCatkinWorkspaceTest):
     def setupWorkspaceContents(self):
         rosinstall(self.workspacedir,
                    os.path.join(os.path.dirname(__file__),
-                                'test.rosinstall-for-test'))
+                                'test.rosinstall'))
         create_catkin_workspace(self.workspacedir)
 
     def tearDown(self):
