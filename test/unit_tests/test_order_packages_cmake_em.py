0ccb873e73c0de7bc20113b4ff4ea5b9c43cc06b
Atsushi Watanabe <atsushi.w@ieee.org>
2025-12-16 18:57:35 +0900

    Fix unit_tests

--- test/unit_tests/test_order_packages_cmake_em.py
+++ test/unit_tests/test_order_packages_cmake_em.py
@@ -17,12 +17,12 @@ class OrderPackagesEmTest(unittest.TestCase):
                  'CMAKE_PREFIX_PATH': ['/bar'],
                  'CATKIN_GLOBAL_LIB_DESTINATION': '/glob-dest/lib',
                  'CATKIN_GLOBAL_BIN_DESTINATION': '/glob-dest/bin',
-                 'PYTHON_INSTALL_DIR': '/foo/dist-packages'}
-        result = em.expand(template, gdict,
-                           source_root_dir='/tmp/nowhere_dir',
-                           whitelisted_packages=[],
-                           blacklisted_packages=[],
-                           underlay_workspaces=[])
+                 'PYTHON_INSTALL_DIR': '/foo/dist-packages',
+                 'source_root_dir': '/tmp/nowhere_dir',
+                 'whitelisted_packages': [],
+                 'blacklisted_packages': [],
+                 'underlay_workspaces': []}
+        result = em.expand(template, gdict)
         self.assertTrue('set(CATKIN_ORDERED_PACKAGES "")' in result, result)
         self.assertTrue('set(CATKIN_ORDERED_PACKAGE_PATHS "")' in result, result)
         self.assertTrue('set(CATKIN_ORDERED_PACKAGES_IS_META "")' in result, result)
