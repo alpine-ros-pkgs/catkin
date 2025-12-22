9175d4d701136b6e236da14bb801f4fdd7ed9a23
Atsushi Watanabe <atsushi.w@ieee.org>
2025-12-22 12:42:46 +0900

    Fix escape

--- cmake/catkin_workspace.cmake
+++ cmake/catkin_workspace.cmake
@@ -64,11 +64,11 @@ function(catkin_workspace)
         if(EXISTS ${CMAKE_SOURCE_DIR}/${path}/CMakeLists.txt)
           # compare CMakeLists.txt with standard content
           file(STRINGS ${CMAKE_CURRENT_BINARY_DIR}/catkin_generated/metapackages/${name}/CMakeLists.txt generated_cmakelists)
-          # allow any of CMake minimum version
-          string(REPLACE "2.8.3" "[0-9]+\\.[0-9]+(\\.[0-9]+)?(\\.\\.\\.[0-9]+\\.[0-9]+(\\.[0-9]+)?)?" generated_cmakelists_pattern "${generated_cmakelists}")
           # need to escape parenthesis
           string(REPLACE "(" "\\(" generated_cmakelists_pattern "${generated_cmakelists_pattern}")
           string(REPLACE ")" "\\)" generated_cmakelists_pattern "${generated_cmakelists_pattern}")
+          # allow any of CMake minimum version
+          string(REPLACE "2.8.3" "[0-9]+\\.[0-9]+(\\.[0-9]+)?(\\.\\.\\.[0-9]+\\.[0-9]+(\\.[0-9]+)?)?" generated_cmakelists_pattern "${generated_cmakelists}")
           file(STRINGS ${path}/CMakeLists.txt existing_cmakelists)
           if(NOT "${existing_cmakelists}" MATCHES "${generated_cmakelists_pattern}")
             set(CATKIN_NONHOMOGENEOUS_WORKSPACE TRUE)
